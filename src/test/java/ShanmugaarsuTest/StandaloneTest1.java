d524a19134cbaa873b98de2180c86c6e335440d2
Shanmugaarasu <akashmoorthy001@gmail.com>
2025-12-19 22:45:31 +0530

    Secondtimeupdate

--- src/test/java/ShanmugaarsuTest/StandaloneTest1.java
+++ src/test/java/ShanmugaarsuTest/StandaloneTest1.java
@@ -17,7 +17,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class StandaloneTest1 {
 
 	public static void main(String[] args) throws InterruptedException {
-         // new comments added
+         // new comments added again
 		String productname = "ZARA COAT 3";
      WebDriverManager.chromedriver().setup();
      WebDriver driver = new ChromeDriver();
